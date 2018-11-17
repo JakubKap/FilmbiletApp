5fa30e9ae40a13d2bb41bca7b498910fee9651e7
Sebastian <folusz.sebastian@gmail.com>
2018-11-18 00:18:20 +0100

    ustawienie paczek, bibliotek, uprawnien

---
+++ android/app/src/main/java/com/companysf/filmbilet/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.companysf.filmbilet.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.companysf.filmbilet.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
