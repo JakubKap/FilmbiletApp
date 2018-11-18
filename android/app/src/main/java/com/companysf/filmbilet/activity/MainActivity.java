01ab1e79c7df151645af91b163a7fd56eb7966e9
Sebastian <folusz.sebastian@gmail.com>
2018-11-18 23:57:27 +0100

    Dodanie logiki dzialania logowania i rejestracji

--- android/app/src/main/java/com/companysf/filmbilet/activity/MainActivity.java
+++ android/app/src/main/java/com/companysf/filmbilet/activity/MainActivity.java
@@ -1,15 +1,41 @@
 package com.companysf.filmbilet.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.companysf.filmbilet.R;
+import com.companysf.filmbilet.addition.SessionManager;
 
 public class MainActivity extends AppCompatActivity {
+    private SessionManager sManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sManager = new SessionManager(getApplicationContext());
+        Button btn_logout = findViewById(R.id.btn_logout);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOutCustomer();
+            }
+        });
+    }
+
+    private void logOutCustomer(){
+        sManager.setLogin(false);
+
+        //TODO remove fields from local databse
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
