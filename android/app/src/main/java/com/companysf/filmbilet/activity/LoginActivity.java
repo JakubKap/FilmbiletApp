a2c5cc317f04aab5f7aa2240414fc7ad8521a8dd
Sebastian <folusz.sebastian@gmail.com>
2018-11-18 15:25:57 +0100

    Ekran logowania, rejestracji

--- android/app/src/main/java/com/companysf/filmbilet/activity/LoginActivity.java
+++ android/app/src/main/java/com/companysf/filmbilet/activity/LoginActivity.java
@@ -2,6 +2,13 @@ package com.companysf.filmbilet.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getActionBar().hide();
+    }
 }
--- android/app/src/main/java/com/companysf/filmbilet/activity/RegisterActivity.java
+++ android/app/src/main/java/com/companysf/filmbilet/activity/RegisterActivity.java
@@ -1,7 +1,14 @@
 package com.companysf.filmbilet.activity;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getActionBar().hide();
+    }
 }
