c7b1b5be964a7ed2d77cce942140909f3e183ce7
Jakub Kapusta <jakubkapusta39@gmail.com>
2018-11-19 22:56:43 +0100

    Zmiana trybu wyświetlania wyboru miejsca - zastosowanie PopupWindow

--- android/app/src/main/java/com/companysf/filmbilet/activity/ChooseSeatActivity.java
+++ android/app/src/main/java/com/companysf/filmbilet/activity/ChooseSeatActivity.java
@@ -1,16 +1,37 @@
 package com.companysf.filmbilet.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
 
 import com.companysf.filmbilet.R;
 
-public class ChooseSeatActivity extends AppCompatActivity {
+public class ChooseSeatActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_seat);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8), (int)(height*.7));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x=0;
+        params.y=-20;
+
+        getWindow().setAttributes(params);
+
+
     }
 }
 
--- android/app/src/main/java/com/companysf/filmbilet/activity/ChooseSeatTypeActivity.java
+++ android/app/src/main/java/com/companysf/filmbilet/activity/ChooseSeatTypeActivity.java
@@ -1,16 +1,33 @@
 package com.companysf.filmbilet.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.companysf.filmbilet.R;
 
 public class ChooseSeatTypeActivity extends AppCompatActivity {
 
+    Button btnNext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_seat_type);
+
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+       btnNext.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent i= new Intent(getApplicationContext(),ChooseSeatActivity.class);
+               startActivity(i);
+           }
+       });
+
+
     }
 }
 
