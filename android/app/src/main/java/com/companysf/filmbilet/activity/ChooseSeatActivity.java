fee691d9556560b0bc4378c2168adc283354f7f5
Jakub Kapusta <jakubkapusta39@gmail.com>
2018-11-19 01:22:26 +0100

    Dodanie layoutu oraz activity odpowiedzialnych za wybranie miejsca w kinie (ChooseSeatActivity.java oraz activity_choose_seat.xml)

---
+++ android/app/src/main/java/com/companysf/filmbilet/activity/ChooseSeatActivity.java
@@ -0,0 +1,16 @@
+package com.companysf.filmbilet.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.companysf.filmbilet.R;
+
+public class ChooseSeatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_seat);
+    }
+}
+
