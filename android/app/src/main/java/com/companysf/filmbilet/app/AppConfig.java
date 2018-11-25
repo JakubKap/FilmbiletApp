b80ce3c4a1e87d4db359beba1b94c53bf4971956
Sebastian <folusz.sebastian@gmail.com>
2018-11-25 16:34:45 +0100

    Dodanie URL API do pobierania filmow

--- android/app/src/main/java/com/companysf/filmbilet/app/AppConfig.java
+++ android/app/src/main/java/com/companysf/filmbilet/app/AppConfig.java
@@ -3,4 +3,5 @@ package com.companysf.filmbilet.app;
 public class AppConfig {
     public static String LOGIN_URL = "http://filmbilet.cba.pl/api/login.php";
     public static String REGISTER_URL = "http://filmbilet.cba.pl/api/register.php";
+    public static String GET_MOVIES_FROM_REPERTOIRE = "http://filmbilet.cba.pl/api/Api.php?q=getMovies&from=repertoire";
 }
