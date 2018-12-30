154bc7e57647c9d6471be3d37f1be794e6231f5c
Sebastian <folusz.sebastian@gmail.com>
2018-12-30 19:32:08 +0100

    Zmiana hostingu na Google Cloud Platform - zmiana stalych logowania do bazy

--- web_service/includes/Constants.php
+++ web_service/includes/Constants.php
@@ -1,7 +1,8 @@
-<?php 
-	header('Content-Type: application/json; charset=utf-8');
 
-	define('DB_HOST', 'mysql.cba.pl');
-	define('DB_USER', 'filmBilet');
-	define('DB_PASS', 'Kuba01Seba');
-	define('DB_NAME', 'seba974');
+<?php 
+        header('Content-Type: application/json; charset=utf-8');
+
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'seba974');
+        define('DB_PASS', 'Kuba01Seba');
+        define('DB_NAME', 'filmBilet');
