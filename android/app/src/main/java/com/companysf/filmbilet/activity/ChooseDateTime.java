8ed6cb701996e6cd2163f3c111b9b721c44b37ec
Jakub Kapusta <jakubkapusta39@gmail.com>
2019-01-11 20:32:35 +0100

    Dodanie nowego Activity i layoutu. Dodanie URL do AppConfig

---
+++ android/app/src/main/java/com/companysf/filmbilet/activity/ChooseDateTime.java
@@ -0,0 +1,15 @@
+package com.companysf.filmbilet.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.companysf.filmbilet.R;
+
+public class ChooseDateTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_date_time);
+    }
+}
--- android/app/src/main/java/com/companysf/filmbilet/app/AppConfig.java
+++ android/app/src/main/java/com/companysf/filmbilet/app/AppConfig.java
@@ -6,5 +6,6 @@ public class AppConfig {
     public static String GET_MOVIES_FROM_REPERTOIRE = "http://35.204.119.131/api/Api.php?q=getMovies&from=repertoire";
     public static String GET_RESERVATIONS = "http://35.204.119.131/api/reservations.php";
     public static String STORE_RESERVATION = "http://35.204.119.131/api/storeReservation.php";
+    public static String GET_MOVIE_REPERTOIRE = "http://35.204.119.131/api/repertoires.php";
     public static String websocketURL = "ws://35.204.119.131:8080/";
 }
